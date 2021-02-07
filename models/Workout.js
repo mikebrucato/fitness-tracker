dc6756cf5a721c9262124cf4b7154df220cae497
mikebrucato <brucatomike1@gmail.com>
2021-02-06 18:44:05 -0600

    create workout schema

---
+++ models/Workout.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const WorkoutSchema = new Schema ({
+    
+        date: {
+            type: Date,
+            default: Date.now
+        },
+
+        exercises: [
+          {
+            type: String,
+            name: String,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number
+          }
+        ]
+    });
+      
+
+const Workout = mongoose.model("Workout", WorkoutSchema)
+
+module.exports = Workout
