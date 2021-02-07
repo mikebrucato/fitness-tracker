45c24fabc1f8124c139cfd5775970b8d34ee7f19
mikebrucato <brucatomike1@gmail.com>
2021-02-06 19:35:32 -0600

    modified html route paths

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,23 +1,25 @@
-const extress = require("express")
+const express = require("express")
 const path = require("path")
-const app = require("express")
+const app = express()
 
 
 // *** HTML routes *** //
 
 // gets index.html
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
 
 // gets exercise.html
 app.get("/exercise", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/exercise.html"))
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
 })
 
 
 // gets stats.html
 app.get("/stats", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/stats.html"))
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
 })
+
+module.exports = app
