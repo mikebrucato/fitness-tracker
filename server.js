0b8a3930cd89acca760bc8f184e3c1ab5ec80557
mikebrucato <brucatomike1@gmail.com>
2021-02-07 19:49:02 -0600

    update for heroku deployment

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 app.use(require("./routes/api-routes.js"));
