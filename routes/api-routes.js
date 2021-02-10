e5a381e3ff926553202574dd5388693f32dea1d4
mikebrucato <brucatomike1@gmail.com>
2021-02-09 22:04:00 -0600

    add get all workouts and create new workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,31 +1,37 @@
 const express = require("express")
 const app = express();
-const db = require("../models");
+const Workout = require("../models/Workout");
 
+
+// gets all workouts
+app.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+      console.log("you've created a new workout!")
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+// creates new workout
 app.post("/api/workouts", ({ body }, res) => {
-    db.create(body)
-      .then(dbWorkouts => {
-        res.json(dbWorkouts);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
-  // add exercises to recent workout plan
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    }).catch(err => {
+      res.statusCode(400).json(err)
+    })
+})
+
+// View the combined weight of multiple exercises from the past seven workouts on the `stats` page.
 
 
 
 
-  // add new exercises to new workout plan
-
-
-
-  // View the combined weight of multiple exercises from the past seven workouts on the `stats` page.
-
-
-
-
-  // View the total duration of each workout from the past seven workouts on the `stats` page.
+// View the total duration of each workout from the past seven workouts on the `stats` page.
 
 
 module.exports = app
